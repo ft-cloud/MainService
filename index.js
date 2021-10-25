aae6c04cc7582cdd819dc00a069a48e1201bd579
Coder246 <46609883+Coder246@users.noreply.github.com>
2021-10-25 17:23:33 +0200

    Added Error handler

--- index.js
+++ index.js
@@ -149,6 +149,15 @@ app.ws('/device/liveconnection', function (ws, req) {
 
 */
 
+app.use(function (err,req,res,next){
+    if (res.headersSent) {
+        return next(err);
+    }
+    res.status(500);
+    res.send('Something went wrong')
+})
+
+
 app.use(function (req, res) {
     res.status(404).send('Something went wrong! Microservice: RegDevice');
 });
